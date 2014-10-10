95dfd0b6e3b482ac6a3a79e4669dfc7fc971f144
hyone <hyone.development@gmail.com>
2014-10-10 20:49:23 +0900

    Fix: no locale on centos causes serverspec failure

--- test/cookbooks/hyone_zsh-test/recipes/_prepare.rb
+++ test/cookbooks/hyone_zsh-test/recipes/_prepare.rb
@@ -10,6 +10,21 @@ when platform?('ubuntu')
   include_recipe 'apt'
 end
 
+# Generate locales to avoid warnings like:
+# 'bash: warning: setlocale: LC_ALL: cannot change locale (ja_JP.UTF-8)'
+case
+when platform?('centos')
+  execute 'generate locale' do
+    command 'localedef -f UTF-8 -i ja_JP /usr/lib/locale/ja_JP.UTF-8'
+    action [:run]
+  end
+when platform?('ubuntu')
+  execute 'locale-gen' do
+    command 'locale-gen ja_JP.UTF-8'
+    action [:run]
+  end
+end
+
 ## user and group
 user _user do
   home _home
