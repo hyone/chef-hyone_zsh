0863179ba28153a07b133fa671cfbd1a4484e444
hyone <hyone.development@gmail.com>
2014-10-10 21:10:29 +0900

    Support CentOS 7

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures zsh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
 depends          'build-essential'
 depends          'hyone_source_package', '>= 0.2.0'
+
+supports         'ubuntu', '12.04'
+supports         'centos', ">= 6"
