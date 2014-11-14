8bf3f8a93e9d4941c581b229b80f648877f8b4df
hyone <hyone.development@gmail.com>
2014-11-14 09:32:12 +0900

    Fix: version specification of 'supports'

--- metadata.rb
+++ metadata.rb
@@ -9,5 +9,5 @@ version          '0.1.0'
 depends          'build-essential'
 depends          'hyone_source_package', '>= 0.2.0'
 
-supports         'ubuntu', '12.04'
-supports         'centos', ">= 6"
+supports         'ubuntu', '>= 12.04'
+supports         'centos', ">= 6.0"
